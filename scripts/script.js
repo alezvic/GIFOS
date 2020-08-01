2a2dc5b2c768a9015c638268236040074a4f8efb
Ale Z <alzuvic@gmail.com>
2020-08-01 20:37:05 -0300

    Add Carousel

--- scripts/script.js
+++ scripts/script.js
@@ -23,12 +23,14 @@ const btn_search = document.getElementById('btn_search');
 const btn_trending = document.getElementById('btn_trending');
 const btn_clean = document.getElementById('btn_clean');
 const div_gifShowcase = document.getElementById('gif_results_showcase');
-const div_trendingLinksContainer = document.getElementById('trending_links_container');
+const div_trendLinksContainer = document.getElementById('trending_links_container');
 const div_trendingGifsContainer = document.getElementById('trending_gifs_container');
 const search_result_toggables = document.getElementsByClassName('search_result_toggable');
 const btn_show_more = document.getElementById('btn_show_more');
 const div_search_suggestions = document.getElementById('div_suggestions');
 const h1_searchQuery = document.getElementById('search_h1');
+const btn_carouselNext = document.getElementById('btn_next');
+const btn_carouselPrevious = document.getElementById('btn_previous')
 
 // Aux
 const btn_autocomplete = document.getElementById('btn_autocomplete');
@@ -39,6 +41,8 @@ const trending_base_url = 'https://api.giphy.com/v1/gifs/trending';
 const trending_searches_base_url = 'https://api.giphy.com/v1/trending/searches';
 const autocomplete_base_url = 'https://api.giphy.com/v1/gifs/search/tags';
 const api_key = '2QRBa2w3k34LbUKfXGoNpuL3Mj6sHAEQ';
+
+var trendingOffset = 0;
 
 //  Fetch data. Returns unparsed, as is, response from URL. 
 //  Use this function to serve ***ANY*** request to Giphy
@@ -70,8 +74,9 @@ async function getAutocompleteSuggestions(query) {
 
 //  Triggers a trending request with $limit number of results. 
 //  Returns *unparsed* response
-async function getTrending(limit = 12) {
-    let response = await fetchData(`${trending_base_url}?api_key=${api_key}&limit=${limit}`);
+async function getTrending(limit = 3, offset = 0) {
+
+    let response = await fetchData(`${trending_base_url}?api_key=${api_key}&limit=${limit}&offset=${offset}`);
 
     return response;
 }
@@ -105,14 +110,15 @@ function addGifs(data) {
     btn_show_more.style.display = 'inline-block';
 }
 
-function addTrendingImgs(data, limit = 3) {
+function addTrendingImgs(data) {
+
+    div_trendingGifsContainer.innerHTML = null;
 
     for (let i = 0; i < data.data.length; i++) {
         let gif_img = document.createElement('img');
         gif_img.setAttribute('src', data.data[i].images.downsized_large.url);
         div_trendingGifsContainer.appendChild(gif_img);
     }
-
 }
 
 function addTrendingLinks(data, limit = 5) {
@@ -125,19 +131,19 @@ function addTrendingLinks(data, limit = 5) {
         span.classList.add('span_trending');
         span.classList.add('capitalized');
         span.innerText = data.data[i];
-        span.addEventListener('click',() => {
+        span.addEventListener('click', () => {
             input_search.value = data.data[i];
-            search(input_search.value).then((response) => { addGifs(response);} );
+            search(input_search.value).then((response) => { addGifs(response); });
         });
 
         if (i > 0 && i != limit) {
             let span_comma = document.createElement('span');
             span_comma.classList.add('span_trending');
             span_comma.innerText = ', ';
-            div_trendingLinksContainer.appendChild(span_comma);
+            div_trendLinksContainer.appendChild(span_comma);
         }
 
-        div_trendingLinksContainer.appendChild(span);
+        div_trendLinksContainer.appendChild(span);
     }
 }
 
@@ -180,15 +186,6 @@ function cleanSuggestions() {
 btn_search.addEventListener('click', () => { search(input_search.value).then(response => { addGifs(response); }); });
 btn_clean.addEventListener('click', () => { cleanAll() });
 
-// btn_autocomplete.addEventListener('click', () => {
-//     getAutocompleteSuggestions(input_search.value)
-//         .then(response => { addSuggestions(response); });
-// });
-
-
-// btn_show_more   .addEventListener('click',  () => { search(input_search.value,12,12).then(response => { addGifs(response); }); });
-
-
 input_search.addEventListener('input', () => {
 
     if (input_search.value.length > 2) {
@@ -199,5 +196,34 @@ input_search.addEventListener('input', () => {
     }
 });
 
+btn_carouselPrevious.addEventListener('click', () => {
+    if (trendingOffset != 0) {
+        if (trendingOffset <= 3) {
+            trendingOffset = 0;
+        }
+        else {
+            trendingOffset -= 3;
+        }
+        getTrending(3, trendingOffset)
+            .then(response => { addTrendingImgs(response); });
+    }
+
+});
+
+btn_carouselNext.addEventListener('click', () => {
+
+    trendingOffset += 3;
+    getTrending(3, trendingOffset)
+        .then(response => { addTrendingImgs(response); });
+});
+
 getTrendingSearches().then(response => { addTrendingLinks(response) });
-getTrending(3).then(response => { addTrendingImgs(response) });
+getTrending().then(response => { addTrendingImgs(response) });
+
+// btn_autocomplete.addEventListener('click', () => {
+//     getAutocompleteSuggestions(input_search.value)
+//         .then(response => { addSuggestions(response); });
+// });
+
+
+// btn_show_more   .addEventListener('click',  () => { search(input_search.value,12,12).then(response => { addGifs(response); }); });
