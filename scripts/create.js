7c1740ca01ad36f4d5cad9bee5c28285447f36db
Ale Z <alzuvic@gmail.com>
2020-08-16 18:38:39 -0300

    Add gif record and upload capabilities

--- scripts/create.js
+++ scripts/create.js
@@ -4,28 +4,77 @@
 // GUI Elements
 const div_cam_preview = document.getElementById('video_cam_preview');
 const btn_record = document.getElementById('btn_record');
-const btn_stop = document.getElementById('bt_stop');
+const btn_stop = document.getElementById('btn_stop');
 const btn_upload = document.getElementById('btn_upload');
+const stream = null;
+let recorder = null;
+let form = new FormData();
 
-async function getStreamAndRecord() {
-    let stream = null;
-    
-    try {
-        stream = await navigator.mediaDevices.getUserMedia({
-            audio: false,
-            video: {
-                height: { max: 480}
-            }
+const upload_url = 'https://upload.giphy.com/v1/gifs';
+
+function record() {
+    navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+        .then(stream => {
+            div_cam_preview.srcObject = stream;
+            div_cam_preview.play()
+            recorder = RecordRTC(stream, {
+                type: 'gif',
+                frameRate: 1,
+                quality: 7,
+                width: 360,
+                hidden: 240,
+                onGifRecordingStarted: function () {
+                    console.log('started')
+                },
+            });
+            recorder.startRecording();
         });
-        div_cam_preview.srcObject = stream;
-        div_cam_preview.play();
-    } 
-    catch(error) {
-        console.log(error);
-    }
 }
 
+
+async function stopStream() {
+
+
+
+    div_cam_preview.srcObject = null;
+    recorder.stopRecording();
+
+    var blob = await recorder.getBlob();
+
+    await form.append('file',blob, 'myGif.gif');
+
+}
+
+async function upload() {
+    let uri = upload_url + '?api_key='+api_key;
+
+    fetch(uri, {
+        method: 'POST',
+        mode: 'cors',
+        body: form
+    })
+    .then(response => response.json())
+    .then(result => {
+        // console.log(`Resultado:${result}`);
+        res = result;
+        console.log(res); 
+        console.log(res['data']['id']);   
+        alert('https://giphy.com/gifs/' + res['data']['id'])    
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+
 btn_record.addEventListener('click', () => {
-    console.log('button');
-    getStreamAndRecord();
+    record();
+});
+
+btn_stop.addEventListener('click', () => {
+    stopStream();
+});
+
+btn_upload.addEventListener('click', () => {
+    upload();
 });
