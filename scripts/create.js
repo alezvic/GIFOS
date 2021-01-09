95f1f4b202a8b48b33b0550931560c4a94bebe5f
Ale Z <alzuvic@gmail.com>
2021-01-09 18:31:55 -0300

    Add capture.html, edit create.js

--- scripts/create.js
+++ scripts/create.js
@@ -1,19 +1,34 @@
 // Camera record, gif creation and upload for GIFOS
 // 
 
+// API key
+const api_key = '2QRBa2w3k34LbUKfXGoNpuL3Mj6sHAEQ';
+
 // GUI Elements
 const div_cam_preview = document.getElementById('video_cam_preview');
+const btn_start = document.getElementById('btn_start');
 const btn_record = document.getElementById('btn_record');
 const btn_stop = document.getElementById('btn_stop');
 const btn_upload = document.getElementById('btn_upload');
-const link_gif = document.getElementById('linkgif');
+
+const steps = document.getElementsByClassName('steps');
+
+const allow_message = document.getElementById('allow_message');
+const preview_message = document.getElementById('preview_message');
+const cam_image = document.getElementById('cam_image');
+
 const stream = null;
 let recorder = null;
 let form = new FormData();
 
 const upload_url = 'https://upload.giphy.com/v1/gifs';
 
-function record() {
+function onLoad() {
+    allow_message.style.display = 'none';
+}
+
+function getMediaDevices() {
+    
     navigator.mediaDevices.getUserMedia({ audio: false, video: true })
         .then(stream => {
             div_cam_preview.srcObject = stream;
@@ -22,54 +37,66 @@ function record() {
                 type: 'gif',
                 frameRate: 1,
                 quality: 7,
-                width: 360,
-                hidden: 240,
+                width: 640,
+                hidden: 480,
                 onGifRecordingStarted: function () {
                     console.log('started')
                 },
             });
-            recorder.startRecording();
-        });
+        })
+        .catch(err => alert('No podemos grabar tu GIFO sin permisos para usar la cámara!'));
 }
 
+function record() {
+    recorder.startRecording();
+}
 
 async function stopStream() {
 
-
-
-    div_cam_preview.srcObject = null;
+    //div_cam_preview.srcObject = null;
+    // TODO can we replay the recorded gif?
     recorder.stopRecording();
 
     var blob = await recorder.getBlob();
 
-    await form.append('file',blob, 'myGif.gif');
+    form.append('file', blob, 'myGif.gif');
 
 }
 
 async function upload() {
-    let uri = upload_url + '?api_key='+api_key;
+    let uri = upload_url + '?api_key=' + api_key;
 
     fetch(uri, {
         method: 'POST',
-        mode: 'cors',
+        //        mode: 'cors',
         body: form
     })
-    .then(response => response.json())
-    .then(result => {
-        // console.log(`Resultado:${result}`);
-        res = result;
-        console.log(res); 
-        console.log(res['data']['id']);   
-        link_gif.setAttribute('href', 'https://giphy.com/gifs/'+res['data']['id']);
-        link_gif.innerText='Ahora si!';
-    })
-    .catch(err => {
-        console.log(err);
-    });
+        .then(response => response.json())
+        .then(result => {
+            // console.log(`Resultado:${result}`);
+            res = result;
+            console.log(res);
+            console.log(res['data']['id']);
+            // link_gif.setAttribute('href', 'https://giphy.com/gifs/'+res['data']['id']);
+            // link_gif.innerText='Ahora si!';
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 
+onLoad()
+
+btn_start.addEventListener('click', ()=> {
+
+    // TODO check if media devices is allowed, to hide back the permissions message
+    preview_message.style.display = 'none';
+    allow_message.style.display = 'block';
+    getMediaDevices();
+});
 
 btn_record.addEventListener('click', () => {
+    allow_message.style.display = 'none';
     record();
 });
 
